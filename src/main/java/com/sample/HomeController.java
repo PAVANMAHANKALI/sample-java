ed50fe0108cd30fdb831260aaf438621d42ef598
PAVANMAHANKALI <32824156+PAVANMAHANKALI@users.noreply.github.com>
2021-11-11 16:40:41 +0530

    second commit

---
+++ src/main/java/com/sample/HomeController.java
@@ -0,0 +1,20 @@
+package com.sample;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/")
+public class HomeController {
+	
+
+	@GetMapping("/index")
+	public String index() {
+	return "Welcome to Index Page";	
+	}
+	@GetMapping("/home")
+	public String home() {
+	return "welcome to Home Page";	
+	}
+	
+}
